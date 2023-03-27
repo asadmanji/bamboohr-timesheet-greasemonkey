04655806a86d0533fb29166e9faeba729e395400
Asad Manji <71824904+asadmanji@users.noreply.github.com>
2023-03-27 10:39:03 +0100

    Update and rename bamboohr-timesheet-data-entry.js to bamboohr-timesheet-data-entry.user.js

--- bamboohr-timesheet-data-entry.user.js
+++ bamboohr-timesheet-data-entry.user.js
@@ -5,5 +5,5 @@
 // @author       Asad Manji
 // @match        https://*.bamboohr.com/employees/timesheet/*
 // @homepageURL  https://github.com/asadmanji/bamboohr-timesheet-greasemonkey/
-// @updateURL    https://raw.githubusercontent.com/sadmanji/bamboohr-timesheet-greasemonkey/main/bamboohr-timesheet-data-entry.js
+// @updateURL    https://raw.githubusercontent.com/sadmanji/bamboohr-timesheet-greasemonkey/main/bamboohr-timesheet-data-entry.user.js
 // ==/UserScript==
