117c5cfae3faa330165cb7f4a541d04cf8823147
Asad Manji <71824904+asadmanji@users.noreply.github.com>
2024-09-06 16:51:38 +0100

    Bump version

--- bamboohr-timesheet-data-entry.user.js
+++ bamboohr-timesheet-data-entry.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         BambooHR Timesheet Data Entry Extension
-// @version      0.10
+// @version      0.11
 // @description  Fill BambooHR Timesheet month with templates, inspired by https://github.com/skgsergio/greasemonkey-scripts
 // @author       Asad Manji
 // @match        https://*.bamboohr.com/employees/timesheet/*
